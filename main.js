809f066c9534ea1b1c77c07a655f11d2ac47922c
David Leonard <davidrobertleonard@gmail.com>
2017-07-15 13:52:05 -0700

    Add copy/paste, fixes #2

--- main.js
+++ main.js
@@ -3,14 +3,36 @@
 const electron = require('electron');
 const ipcMain = electron.ipcMain;
 const clipboard = electron.clipboard;
+const Menu = electron.Menu;
 const menubar = require('menubar');
 
 const mb = menubar({
   'node-integration' : true
 });
 
+const app = mb.app;
+
 mb.on('ready', function ready() {
-  console.log('app is ready')
+  console.log('app is ready');
+
+  // Creates an application "menu", even though we'll never see it since we're
+  // in tray app mode. Necessary to enable copy/paste on OSX:
+  // https://pracucci.com/atom-electron-enable-copy-and-paste.html
+  const template = [
+    {
+      label: "Application",
+      submenu: []
+    },
+    {
+      label: "Edit",
+      submenu: [
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" }
+      ]
+    }
+  ];
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 });
 
 mb.on('after-create-window', function afterCreateWindow() {
